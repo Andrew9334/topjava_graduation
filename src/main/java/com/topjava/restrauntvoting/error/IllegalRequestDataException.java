35b7b9323c739fd92b97c2220e4ea03a9aa6f897
Andrew <andfrewluferenko@icloud.com>
2024-03-08 16:51:35 +0300

    created package error

---
+++ src/main/java/com/topjava/restrauntvoting/error/IllegalRequestDataException.java
@@ -0,0 +1,9 @@
+package com.topjava.restrauntvoting.error;
+
+public class IllegalRequestDataException extends AppException {
+
+    public IllegalRequestDataException(String msg) {
+        super(msg);
+    }
+
+}
