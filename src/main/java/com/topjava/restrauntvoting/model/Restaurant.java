295efc174178da9db954ef2a5b8b3c5bc7b630c0
Andrew <andfrewluferenko@icloud.com>
2024-03-09 15:41:59 +0300

    fix classes Restaurant, User, create class Vote, added table Vote to data.sql

--- src/main/java/com/topjava/restrauntvoting/model/Restaurant.java
+++ src/main/java/com/topjava/restrauntvoting/model/Restaurant.java
@@ -1,10 +1,13 @@
 package com.topjava.restrauntvoting.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.util.List;
 
@@ -17,7 +20,6 @@ public class Restaurant extends NamedEntity{
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "restaurant")
     @OrderBy("name asc")
-//    @OnDelete(action = OnDeleteAction.CASCADE)
     private List<Dishes> dishes;
 
     public Restaurant(Integer id, String name, List<Dishes> dishes) {
