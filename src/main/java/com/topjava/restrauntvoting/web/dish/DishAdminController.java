048747e60891cb04e6577a9ce42a302d090f6887
Andrew <andfrewluferenko@icloud.com>
2024-03-12 17:01:02 +0300

    added method update and create to DishAdminController

--- src/main/java/com/topjava/restrauntvoting/web/dish/DishAdminController.java
+++ src/main/java/com/topjava/restrauntvoting/web/dish/DishAdminController.java
@@ -11,8 +11,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+
+import static com.topjava.restrauntvoting.validation.ValidationUtil.assureIdConsistent;
+import static com.topjava.restrauntvoting.validation.ValidationUtil.checkNew;
 
 @RestController
 @RequestMapping(value = DishAdminController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -44,7 +49,22 @@ public class DishAdminController {
         return repository.getAll(restaurantId);
     }
 
+    @PutMapping(value = "{/id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@PathVariable("restaurantId") int restaurantId, @PathVariable("id") int id, @Valid @RequestBody Dish dish) {
         log.info("update {} for restaurant {}", dish, restaurantId);
+        assureIdConsistent(dish, id);
+        repository.getBelonged(restaurantId, id);
+        service.save(restaurantId, dish);
+    }
+
+    public ResponseEntity<Dish> create(@PathVariable("restaurantId") int restaurantId, @Valid @RequestBody Dish dish) {
+        log.info("create {} for restaurant {}", dish, restaurantId);
+        checkNew(dish);
+        Dish created = service.save(restaurantId, dish);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
     }
 }
