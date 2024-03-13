5e5ce630e49e896450525980cf22d79950e73725
Andrew <andfrewluferenko@icloud.com>
2024-03-13 15:38:24 +0300

    fixed RestaurantRepository

--- src/main/java/com/topjava/restrauntvoting/repository/RestaurantRepository.java
+++ src/main/java/com/topjava/restrauntvoting/repository/RestaurantRepository.java
@@ -1,10 +1,19 @@
 package com.topjava.restrauntvoting.repository;
 
+import com.topjava.restrauntvoting.error.DataConflictException;
 import com.topjava.restrauntvoting.model.Restaurant;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Transactional(readOnly = true)
 public interface RestaurantRepository extends BaseRepository<Restaurant> {
+
+    @Query("SELECT r FROM Restaurant r WHERE r.id=:id")
+    Optional<Restaurant> get(int id);
+
+    @Query("SELECT r FROM Restaurant r WHERE r.id=:id")
+    List<Restaurant> getAll(int id);
 }
