bd31fb5075900535c3b72784aca7cb40aed1689c
Andrew <andfrewluferenko@icloud.com>
2024-03-14 17:20:39 +0300

    fixed getBelonged

--- src/main/java/com/topjava/restrauntvoting/repository/VoteRepository.java
+++ src/main/java/com/topjava/restrauntvoting/repository/VoteRepository.java
@@ -4,10 +4,12 @@ import com.topjava.restrauntvoting.error.DataConflictException;
 import com.topjava.restrauntvoting.model.Vote;
 import com.topjava.restrauntvoting.to.VoteTo;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional(readOnly = true)
 public interface VoteRepository extends BaseRepository<Vote> {
 
     @Query("SELECT v FROM Vote v WHERE v.user.id=:userId ORDER BY v.dateTime DESC")
@@ -22,8 +24,8 @@ public interface VoteRepository extends BaseRepository<Vote> {
     @Query("SELECT v FROM Vote v JOIN FETCH v.user AND JOIN FETCH v.restaurant WHERE v.id=:id AND v.user.id=:userId AND v.restaurant.id=:restaurantId")
     Optional<Vote> getWithUserAndRestaurant(int id, int userId, int restaurantId);
 
-    default Vote getBelonged(int userId, int id) {
-        return get(userId, id).orElseThrow(
+    default Vote getBelonged(int userId, int restaurantId, int id) {
+        return get(userId,  id).orElseThrow(
                 () -> new DataConflictException("Entity id=" + id + "   is not exist or doesn't belong to User id=" + userId));
     }
 }
