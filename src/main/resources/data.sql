bf263fac5335b99b0b174b363dbb250ef0ae5213
Andrew <andfrewluferenko@icloud.com>
2024-03-26 23:02:27 +0300

    corrected and deleted unnecessary

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,29 +1,3 @@
--- INSERT INTO USERS (NAME, EMAIL, PASSWORD)
--- VALUES ('User', 'user@yandex.ru', '{noop}password'),
---        ('Admin', 'admin@gmail.com', '{noop}admin'),
---        ('Guest', 'guest@gmail.com', '{noop}guest');
---
--- INSERT INTO USER_ROLE (ROLE, USER_ID)
--- VALUES ('USER', 1),
---        ('ADMIN', 2),
---        ('USER', 2);
---
--- INSERT INTO RESTAURANT (NAME)
--- VALUES ('Piatsa Gourounaki'),
---        ('Ladi and Rigani'),
---        ('The Hill View restaurant'),
---        ('Sage Restaurant');
---
--- INSERT INTO MENU (restaurant_id, name, date, description, price)
--- VALUES (1,'Piatsa Gourounaki', '2024-03-08', 'Meze', 20),
---        (1, 'Piatsa Gourounaki', '2024-03-08', 'Mahalembi', 40),
---        (1, 'Piatsa Gourounaki', '2024-03-08', 'Soup', 10),
---        (2, 'Ladi and Rigani', '2024-03-09', 'Сhicken gyro', 20),
---        (2, 'Ladi and Rigani', '2024-03-09', 'Sheftalyu', 50),
---        (2, 'Ladi and Rigani', '2024-03-09', 'Souvlaki', 35),
---        (2, 'Ladi and Rigani', '2024-03-09', 'Souvlaki', 35);
-
-
 INSERT INTO USERS (NAME, EMAIL, PASSWORD)
 VALUES ('User', 'user@yandex.ru', '{noop}password'),
        ('Admin', 'admin@gmail.com', '{noop}admin'),
