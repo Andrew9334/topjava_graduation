5706e3e4854d9081abffbece51c4435b5a08f887
Andrew <andfrewluferenko@icloud.com>
2024-03-27 19:03:05 +0300

    corrected data.sql, remove milliseconds

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -22,7 +22,7 @@ VALUES ('Beef steak', current_date, 500, 1),
        ('Grilled ribs', current_date, 250, 3);
 
 INSERT INTO VOTE (user_id, restaurant_id, date_time)
-VALUES (1, 1, current_timestamp()),
-       (2, 2, current_timestamp()),
-       (2, 1, current_timestamp()),
-       (1, 3, current_timestamp());
+VALUES (1, 1, current_timestamp(0)),
+       (2, 2, current_timestamp(0)),
+       (2, 1, current_timestamp(0)),
+       (1, 3, current_timestamp(0));
